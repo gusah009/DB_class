ae289b77c3b695399a23106a292f973f8a4d5b23
gram_jhm <gusah009@naver.com>
2021-12-22 02:31:22 +0900

    미사용 폴더 제거

--- project/category/myApp/forms.py
+++
@@ -1,9 +0,0 @@
-from django import forms
-from .models import Students
-
-class PostForm(forms.ModelForm):
-  
-  class Meta:
-
-    model = Students
-    fields = ['id', 'firstname', 'secondname', 'age', 'major', 'address']
